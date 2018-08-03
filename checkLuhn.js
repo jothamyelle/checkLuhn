2316aad00b98329d47b5234fb390479a3403dc2b
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-03 22:47:06 +0000

    Added a get check digit function and summed up all the digits in the array

--- checkLuhn.js
+++ checkLuhn.js
@@ -8,11 +8,32 @@ function checkLuhn(num) {
         numArray[i] *= 2;
     }
     // Sum all the individual digits (digits in parentheses are the products from Step 1): x (the check digit) + (2) + 7 + (1+6) + 9 + (6) + 7 + (4) + 9 + (1+8) + 7 = x + 67.
-    
-    // If the sum is a multiple of 10, the account number is possibly valid. Note that 3 is the only valid digit that produces a sum (70) that is a multiple of 10.
-    // Thus these account numbers are all invalid except possibly 79927398713 which has the correct check digit.
+    var sum = 0;
+    for (i in numArray) {
+        if (numArray[i] > 9) {
+            var newNumArray = (numArray[i]).toString().split("");
+            var newNumber = 0;
+            for(digit in newNumArray) {
+                newNumArray[digit] = parseFloat(newNumArray[digit]);
+                newNumber += newNumArray[digit];
+            }
+            numArray[i] = newNumber;
+        }
+        sum += numArray[i];
+    }
+    var checkDigit = getCheckDigit(sum);
+    sum += checkDigit;
+    console.log(sum);
 
-    return numArray;
+    // If the sum is a multiple of 10, the account number is possibly valid. Note that 3 is the only valid digit that produces a sum (70) that is a multiple of 10.
+
+    return "This is a vlid number";
+}
+
+function getCheckDigit(sum){
+    var totalArray = (sum * 9).toString().split("");
+    var cD = parseFloat(totalArray[totalArray.length - 1]);
+    return cD
 }
 
 
