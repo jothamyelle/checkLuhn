0d2091241211dd5fec36c3f35bc4b378d1edf09f
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-03 22:54:24 +0000

    checks the check digit and adds it to sum, if it mod 10 to 0, then we're good

--- checkLuhn.js
+++ checkLuhn.js
@@ -3,11 +3,13 @@ function checkLuhn(num) {
     var numArray = num.split("").map(i => {
         return parseFloat(i);
     });
-    // Double every second digit, from the rightmost: (1×2) = 2, (8×2) = 16, (3×2) = 6, (2×2) = 4, (9×2) = 18
+
+    // Double every second digit
     for (var i = numArray.length - 1; i >= 0; i = i - 2) {
         numArray[i] *= 2;
     }
-    // Sum all the individual digits (digits in parentheses are the products from Step 1): x (the check digit) + (2) + 7 + (1+6) + 9 + (6) + 7 + (4) + 9 + (1+8) + 7 = x + 67.
+    
+    // Sum all the individual digits
     var sum = 0;
     for (i in numArray) {
         if (numArray[i] > 9) {
@@ -23,11 +25,13 @@ function checkLuhn(num) {
     }
     var checkDigit = getCheckDigit(sum);
     sum += checkDigit;
-    console.log(sum);
 
-    // If the sum is a multiple of 10, the account number is possibly valid. Note that 3 is the only valid digit that produces a sum (70) that is a multiple of 10.
-
-    return "This is a vlid number";
+    // If the sum is a multiple of 10, the account number is possibly valid
+    if (sum % 10 == 0) {
+        return "This is a valid number";
+    } else {
+        return "Sorry, dude, your card is no good!"
+    }
 }
 
 function getCheckDigit(sum){
